38161444ab8b730d0d47a6c02f520dc1c42d4dd4
Coralee Long <coraleelong@gmail.com>
2024-12-09 16:30:27 +0100

    add methed getOrdersByOrderStatus to ShopService

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -3,8 +3,13 @@ import java.util.List;
 import java.util.UUID;
 
 public class ShopService {
-    private ProductRepo productRepo = new ProductRepo();
-    private OrderRepo orderRepo = new OrderMapRepo();
+    private ProductRepo productRepo;
+    private OrderRepo orderRepo;
+
+    public ShopService(ProductRepo productRepo, OrderRepo orderRepo) {
+        this.productRepo = productRepo;
+        this.orderRepo = orderRepo;
+    }
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
@@ -17,8 +22,19 @@ public class ShopService {
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+   //   Write a method that returns a list of all orders with a specific order status (parameter) using streams.
+    public List<Order> getOrdersByOrderStatus(OrderStatus orderStatus) {
+        // get all the orders from repo
+        List<Order> orders = orderRepo.getOrders();
+
+        // filter by orderStatus
+        return orders.stream()
+                .filter(order -> order.status().equals(orderStatus))
+                .toList();
     }
 }

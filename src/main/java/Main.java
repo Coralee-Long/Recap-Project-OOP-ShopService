d4a7fbaafa84766418f4b8682a6e3781bf2190bf
Coralee Long <coraleelong@gmail.com>
2024-12-09 17:55:48 +0100

    Test signed commit with SSH

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Step 1: Set up repositories using MockData
+
         ProductRepo productRepo = MockData.setupProductRepo();
         OrderRepo orderRepo = MockData.setupOrderRepo(productRepo);
 
-        // Step 2: Create ShopService
         ShopService shopService = new ShopService(productRepo, orderRepo);
 
-        // Step 3: Call the WIP function to test it
         List<Order> filteredOrders = shopService.getOrdersByOrderStatus(OrderStatus.PROCESSING);
         System.out.println("Filtered Orders: " + filteredOrders);
     }
